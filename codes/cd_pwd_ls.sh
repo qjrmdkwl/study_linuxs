9e6b81c2ad5a3a0fb4560a48e1c987a7b97b8ce1
qjrmdkwl <qjrmdkwl@gmail.com>
2025-07-14 18:01:36 +0900

    Create cd_pwd_ls.sh

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,36 @@
+[chohyunjun@localhost ~]$ pwd
+/home/chohyunjun
+[chohyunjun@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[chohyunjun@localhost ~]$ cd
+[chohyunjun@localhost ~]$ cd
+[chohyunjun@localhost ~]$ cd
+[chohyunjun@localhost ~]$ ^c
+d
+bash: d: command not found...
+[chohyunjun@localhost ~]$ pwd
+/home/chohyunjun
+[chohyunjun@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[chohyunjun@localhost ~]$ cd
+[chohyunjun@localhost ~]$ cd /home/chohyunjun
+[chohyunjun@localhost ~]$ cd /home/chohyunjun/Downloads
+[chohyunjun@localhost Downloads]$ pwd
+/home/chohyunjun/Downloads
+[chohyunjun@localhost Downloads]$ ls
+[chohyunjun@localhost Downloads]$ pwd
+/home/chohyunjun/Downloads
+[chohyunjun@localhost Downloads]$ cd home/chohyunjun/Music
+bash: cd: home/chohyunjun/Music: No such file or directory
+[chohyunjun@localhost Downloads]$ pwd
+/home/chohyunjun/Downloads
+[chohyunjun@localhost Downloads]$ cd
+[chohyunjun@localhost ~]$ cd /home/chohyunjun/Music
+[chohyunjun@localhost Music]$ pwd
+/home/chohyunjun/Music
+[chohyunjun@localhost Music]$ ls
+[chohyunjun@localhost Music]$ cd /home/chohyunjun/Pictures
+[chohyunjun@localhost Pictures]$ pwd
+/home/chohyunjun/Pictures
+[chohyunjun@localhost Pictures]$ ls
+[chohyunjun@localhost Pictures]$ 
