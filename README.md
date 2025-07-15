3b8480dea867b6646534829212adb15e17a90e9a
qjrmdkwl <qjrmdkwl@gmail.com>
2025-07-15 14:32:59 +0900

    file & directory

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # study_linuxs
 ## powershell (GUI & CLI)
 연습 글을 올립니다.
-- 첫번째
-- 두번째
+- serching command : [안내글](codes/10_powershell.sh)
+- file & directory command : [명령어](codes/20_control_file_dir_powershell.sh)
 
-[안내글](codes/10_powershell.sh)
+## quest
--- codes/20_control_file_dir_powershell.sh
+++ codes/20_control_file_dir_powershell.sh
@@ -1,27 +1,3 @@
-
-PS C:\Users\Administrator> cd C:\
-PS C:\> cd C:\Develops
-PS C:\Develops> pwd
-
-Path
-----
-C:\Develops
-
-
-PS C:\Develops> ls
-
-
-    디렉터리: C:\Develops
-
-
-Mode                 LastWriteTime         Length Name
-----                 -------------         ------ ----
-d-----      2025-07-14  오전 11:59                Downloads
-d-----      2025-07-15  오전 11:42                VMs
-
-
-PS C:\Develops>
-
 PS C:\Develops\tempo_dir> mkdir dir_first
 
 
